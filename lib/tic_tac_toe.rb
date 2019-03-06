85126ae5f08ff5b9ed34bea3f15665109f25e03f
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 03:14:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -101,7 +101,7 @@ class TicTacToe
     end
         if won?
           win_combo = won?
-          puts "Congratulations #{winner(board)}!"
+          puts "Congratulations #{winner    }!"
         elsif draw?
           puts "Cat's Game!"
         end
