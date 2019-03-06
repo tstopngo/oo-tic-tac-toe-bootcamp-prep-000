8b3a2c4764d77bab837fde5b7203d592105cdf8d
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 03:26:02 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,6 +6,17 @@ class TicTacToe
   
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
   
+    def play
+    until over? == true
+      turn
+    end
+        if won?
+          puts "Congratulations #{winner}!"
+        elsif draw?
+          puts "Cat's Game!"
+        end
+  end
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -95,15 +106,6 @@ class TicTacToe
     end
   end
   
-  def play
-    until over? == true
-      turn
-    end
-        if won?
-          puts "Congratulations #{winner}!"
-        elsif draw?
-          puts "Cat's Game!"
-        end
-  end
+
   
 end
