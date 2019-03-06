1d73e84b820f8d4f7b3380c67b32a931590616ae
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 02:46:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,10 +15,10 @@ class TicTacToe
   end
   
   def input_to_index(user_input)
-    user_input.to_i - 1
+    @index=user_input.to_i - 1
   end
   
-  def move(@board[index], token == X)
+  def move(@board[@index], token == X)
   
   end
 
