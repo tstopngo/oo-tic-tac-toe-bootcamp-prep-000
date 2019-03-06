f0ddfb7c0a1f5257f63d1763f37f7c2b3d2b7217
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 02:54:30 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,12 +19,9 @@ class TicTacToe
   end
   
   def move(index, token == X)
-    @board[@index] 
+    @board[@index] = token
   end
 
-  def move(board, index, token)
-  board[index] = token
-  end
 
   def position_taken?(board, index)
     !(board[index].nil? || board[index] == " ")
