b1358009507f26a932b2eda3d5cd89846829bc5e
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 03:33:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,7 +30,7 @@ class TicTacToe
   end
   
   def valid_move?(index)
-    index.between?(0,8) && !position_taken?(@board, index)
+    index.between?(0,8) && !position_taken?(index)
   end
   
   def turn
