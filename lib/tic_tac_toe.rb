bde9d6bb95f7a36eedbc1ef358c2216a4fa0fae1
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 03:06:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,7 @@ class TicTacToe
   
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
   
-  def display_board(board)
+  def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -23,8 +23,8 @@ class TicTacToe
   end
 
 
-  def position_taken?(@board, index)
-    !(@board[index].nil? || board[index] == " ")
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
   end
   
   def valid_move?(@board, index)
